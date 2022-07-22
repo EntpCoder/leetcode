2af6ca4dc7988e4b8950f88ea6d20641777cc886
Yang Song <entpcoder@gmail.com>
2022-07-22 19:50:59 +0800

    "逆波兰表达式 (栈) 未完成"

---
+++ src/main/java/com/songyang/question/Q150.java
@@ -0,0 +1,17 @@
+package com.songyang.question;
+
+/**
+ * @author Yang Song
+ * @date 2022/7/22 17:02
+ */
+public class Q150 {
+    /**
+     * 根据 逆波兰表示法，求表达式的值。
+     * 有效的算符包括+、-、*、/。每个运算对象可以是整数，也可以是另一个逆波兰表达式。
+     * 注意两个整数之间的除法只保留整数部分。
+     * 可以保证给定的逆波兰表达式总是有效的。换句话说，表达式总会得出有效数值且不存在除数为 0 的情况。
+     */
+    public int evalRPN(String[] tokens) {
+        return 0;
+    }
+}
