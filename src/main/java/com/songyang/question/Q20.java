2f9bd2eb77449779f6b62aff0420ba81ecd676de
Yang Song <entpcoder@gmail.com>
2022-07-22 16:26:46 +0800

    "简化路径(栈)"

---
+++ src/main/java/com/songyang/question/Q20.java
@@ -0,0 +1,20 @@
+package com.songyang.question;
+
+/**
+ * @author Yang Song
+ * @date 2022/7/22 16:23
+ */
+public class Q20 {
+    /**
+     * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+     * 有效字符串需满足：
+     * 左括号必须用相同类型的右括号闭合。
+     * 左括号必须以正确的顺序闭合。
+     * @param s 待检验字符串
+     * @return 是否是有效括号
+     */
+    public boolean isValid(String s) {
+
+        return false;
+    }
+}
