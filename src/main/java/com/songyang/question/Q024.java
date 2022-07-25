d30645b878710554ecee0348cba880254e84a264
Yang Song <entpcoder@gmail.com>
2022-07-25 19:11:22 +0800

    "两两交换中间的节点(迭代)"

--- src/main/java/com/songyang/question/Q024.java
+++ src/main/java/com/songyang/question/Q024.java
@@ -7,7 +7,7 @@ import com.songyang.util.node.ListNodeUtils;
  * @author Yang Song
  * @date 2022/7/25 14:43
  */
-public class Q24 {
+public class Q024 {
     /**
      * 给你一个链表，两两交换其中相邻的节点，并返回交换后链表的头节点。
      * 你必须在不修改节点内部的值的情况下完成本题（即，只能进行节点交换）。
