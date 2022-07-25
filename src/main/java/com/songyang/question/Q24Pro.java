841377c08f06204642a163ef8b6f3ef9bd8e8c7a
Yang Song <entpcoder@gmail.com>
2022-07-25 15:33:11 +0800

    "两两交换链表中的节点(迭代)-语法优化版"

--- src/main/java/com/songyang/question/Q24Pro.java
+++ src/main/java/com/songyang/question/Q24Pro.java
@@ -8,6 +8,18 @@ import com.songyang.util.node.ListNodeUtils;
  * @date 2022/7/25 15:29
  */
 public class Q24Pro {
+    /**
+     * 给你一个链表，两两交换其中相邻的节点，并返回交换后链表的头节点。
+     * 你必须在不修改节点内部的值的情况下完成本题（即，只能进行节点交换）。
+     * 1->2->3->4
+     * 2->1->4->3
+     * 思路
+     * 1.前置节点连接到node2上
+     * 2.node1.next = node2.next;
+     * 3.node2.next = node1
+     * 4.将node1保存成前置节点 用于下一次交换 连接node2
+     * 语法优化版本
+     */
     public static ListNode swapPairs(ListNode head) {
         if(head == null){
             return null;
