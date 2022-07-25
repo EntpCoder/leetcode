a42152e72d39af9bb9710f17ebc7da94591b37e5
Yang Song <entpcoder@gmail.com>
2022-07-25 19:52:34 +0800

    "简化路径(栈)"

---
+++ src/main/java/com/songyang/question/Q739.java
@@ -0,0 +1,17 @@
+package com.songyang.question;
+
+/**
+ * @author Yang Song
+ * @date 2022/7/25 19:14
+ */
+public class Q739 {
+    public int[] dailyTemperatures(int[] temperatures) {
+        /*
+            0. 6 5 4 3 2 1 7 没遇到7时需要一直存 直到遇到7才出结果
+            1.当前温度没有遇到比它大的需要保存
+            2.温度变量应该是正着存 倒着出结果的  栈
+
+         */
+        return null;
+    }
+}
