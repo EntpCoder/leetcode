d61fcdcb69c6137f0ec4ac218dd17b6d9d79f155
Yang Song <entpcoder@gmail.com>
2022-07-26 21:24:29 +0800

    "滑动窗口最大值(未完成)"

---
+++ src/main/java/com/songyang/question/Q239.java
@@ -0,0 +1,12 @@
+package com.songyang.question;
+
+/**
+ * @author Yang Song
+ * @date 2022/7/26 19:19
+ */
+public class Q239 {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+         // 如果看到的元素比队尾大 那么队尾的元素一定不是窗口中的最大元素,这个元素就应该移除队尾
+        return new int[0];
+    }
+}
