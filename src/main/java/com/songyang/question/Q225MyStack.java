46a9f330e9b712b4c479cc3b90afc09fc5bcac31
Yang Song <entpcoder@gmail.com>
2022-07-26 19:07:33 +0800

    "用栈实现队列(单个栈)"

--- src/main/java/com/songyang/question/Q225MyStack.java
+++ src/main/java/com/songyang/question/Q225MyStack.java
@@ -1,23 +1,30 @@
 package com.songyang.question;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /**
  * @author Yang Song
  * @date 2022/7/26 15:19
  */
 public class Q225MyStack {
+    Deque<Integer> queue = new ArrayDeque<>();
     public void push(int x) {
-
+        int length = queue.size();
+        queue.addLast(x);
+        for(int i = 0;i < length;i++){
+            queue.addLast(queue.poll());
+        }
     }
-
     public int pop() {
-
+        return queue.poll();
     }
 
     public int top() {
-
+        return queue.peekFirst();
     }
 
     public boolean empty() {
-
+        return queue.isEmpty();
     }
 }
